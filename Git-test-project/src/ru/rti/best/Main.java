2994f6f7aa96f2dfe432982c9a3655c00732df14
Андрей <@Home-pc>
2017-02-18 19:21:33 +0300

    changed text of message

--- Git-test-project/src/ru/rti/best/Main.java
+++ Git-test-project/src/ru/rti/best/Main.java
@@ -3,7 +3,7 @@ package ru.rti.best;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub");
+		System.out.println("Hello second test");
 			
 		}
 	}
